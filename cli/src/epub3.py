6449aaaf04abe4c358123058f67e40e9542ec7ce
Moses Lara <mlara@dealerinspire.com>
2024-11-24 16:35:39 -0600

    refactor: refactor EPUB cover handling and introduce XML utility class

--- cli/src/epub3.py
+++ cli/src/epub3.py
@@ -1,10 +1,12 @@
 import os, typer, subprocess
-from lxml import etree
-from lxml.etree import _ElementTree, parse, _Element
+from cli.src.xml import Xml
+from lxml.etree import _Element
 
 class Epub3:
+    """Class to handle EPUB3 files"""
     def __init__(self, workspace: str = "epub-unzipped"):
         self.workspace = workspace
+        self.xml = Xml()
         
     def init(self, epub_path: str):
         workspace = "epub-unzipped"
@@ -21,18 +23,7 @@ class Epub3:
         else:
             typer.secho(message="Workspace already exists", fg='yellow')
 
-    def load_xml(self, file_path: str) -> _ElementTree:
-        """Load an XML file and return the tree"""
-        tree= parse(file_path)
-        return tree
-
-    def process_tree(self, tree: _ElementTree) -> _Element:
-        """Process the tree and return the root element"""
-        root = tree.getroot()
-        return root
-
     def load_content_opf(self, content_opf_path: str) -> _Element:
         """Load the content.opf file and return the tree"""
-        tree = self.load_xml(content_opf_path + "/content.opf")
-        root = self.process_tree(tree)
-        return root
+        self.xml.load_xml(content_opf_path)
+        self.xml.get_tree_root()
