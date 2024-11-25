334945b1392de5eadde35264a4498ac5c4fbba40
Moses Lara <mlara@dealerinspire.com>
2024-11-24 23:30:30 -0600

    refactor: add setup message for EPUB file editing in setup command

--- cli/commands/setup.py
+++ cli/commands/setup.py
@@ -1,5 +1,6 @@
-import typer, os
+import typer
 from cli.src.epub3 import Epub3
+from cli.src.utils import success
 
 class Setup:
     def main(self, epub_path: str = typer.Argument(
@@ -7,4 +8,6 @@ class Setup:
         help="The path to the epub file"
         )
     ):
+        """Setup an EPUB file for editing"""
+        success(f"Setting up the EPUB file for editing...")
         Epub3().init(epub_path)
